08d28b11f3166bd3aa08a2d88912595e10dc3f87
lucabindertbz <luca.binder@edu.tbz.ch>
2023-01-31 09:42:05 +0100

    worked on the movie project

--- movie_library/routes.py
+++ movie_library/routes.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template, session, redirect, request
+import uuid
+from flask import Blueprint, render_template, session, redirect, request, current_app, url_for
 from movie_library.forms import MovieForm
+from movie_library.models import Movie
+from dataclasses import asdict
 
 pages = Blueprint(
     "pages", __name__, template_folder="templates", static_folder="static"
@@ -18,10 +21,20 @@ def index():
 def add_movie():
     form = MovieForm()
 
-    if request.method == "POST":
-        pass
+    if form.validate_on_submit():
+        movie = Movie(
+            _id=uuid.uuid4().hex,
+            title=form.title.data,
+            director=form.director.data,
+            year=form.year.data
+        )
+
+        current_app.db.movie.insert_one(asdict(movie))
+
+        return redirect(url_for(".index"))
 
     return render_template("new_movie.html", title="Movies Watchlist - Add Movie", form=form)
+
 
 @pages.get("/toggle-theme")
 def toggle_theme():
