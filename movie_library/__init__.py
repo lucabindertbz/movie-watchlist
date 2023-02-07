00802193c96207b15ce1dc152d712a4cfae7bf3b
lucabindertbz <luca.binder@edu.tbz.ch>
2023-02-07 13:52:39 +0100

    added comments to the code

--- movie_library/__init__.py
+++ movie_library/__init__.py
@@ -1,20 +1,30 @@
+# import necessary modules
 import os
 from flask import Flask
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
+# import blueprint for routes
 from movie_library.routes import pages
 
+# load environment variables from .env file
 load_dotenv()
 
 
+# function to create Flask application
 def create_app():
+    # create Flask app
     app = Flask(__name__)
+
+    # set environment variables for the database URI and secret key
     app.config["MONGODB_URI"] = os.environ.get("MONGODB_URI")
-    app.config["SECRET_KEY"] = os.environ.get(
-        "SECRET_KEY", "pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw"
-    )
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw")
+
+    # connect to MongoDB database using the URI and get the default database
     app.db = MongoClient(app.config["MONGODB_URI"]).get_default_database()
 
+    # register blueprint for routes
     app.register_blueprint(pages)
+
+    # return the Flask app
     return app
--- movie_library/forms.py
+++ movie_library/forms.py
@@ -1,4 +1,7 @@
+# Import FlaskForm class from the flask_wtf library
 from flask_wtf import FlaskForm
+
+# Import various field types and validators from the wtforms library
 from wtforms import (
     IntegerField,
     PasswordField,
@@ -7,7 +10,6 @@ from wtforms import (
     TextAreaField,
     URLField,
 )
-
 from wtforms.validators import (
     InputRequired,
     Email,
@@ -17,15 +19,21 @@ from wtforms.validators import (
 )
 
 
+# Define LoginForm class as a subclass of FlaskForm
 class LoginForm(FlaskForm):
+    # Define email field with a label "Email" and validators InputRequired and Email
     email = StringField("Email", validators=[InputRequired(), Email()])
+    # Define password field with a label "Password" and validator InputRequired
     password = PasswordField("Password", validators=[InputRequired()])
+    # Define submit button with a label "Login"
     submit = SubmitField("Login")
 
 
+# Define RegisterForm class as a subclass of FlaskForm
 class RegisterForm(FlaskForm):
+    # Define email field with a label "Email" and validators InputRequired and Email
     email = StringField("Email", validators=[InputRequired(), Email()])
-
+    # Define password field with a label "Password" and validators InputRequired and Length
     password = PasswordField(
         "Password",
         validators=[
@@ -37,7 +45,7 @@ class RegisterForm(FlaskForm):
             ),
         ],
     )
-
+    # Define confirm_password field with a label "Confirm Password" and validators InputRequired and EqualTo
     confirm_password = PasswordField(
         "Confirm Password",
         validators=[
@@ -48,14 +56,17 @@ class RegisterForm(FlaskForm):
             ),
         ],
     )
-
+    # Define submit button with a label "Register"
     submit = SubmitField("Register")
 
 
+# Define MovieForm class as a subclass of FlaskForm
 class MovieForm(FlaskForm):
+    # Define title field with a label "Title" and validator InputRequired
     title = StringField("Title", validators=[InputRequired()])
+    # Define director field with a label "Director" and validator InputRequired
     director = StringField("Director", validators=[InputRequired()])
-
+    # Define year field with a label "Year" and validators InputRequired and NumberRange
     year = IntegerField(
         "Year",
         validators=[
@@ -63,30 +74,50 @@ class MovieForm(FlaskForm):
             NumberRange(min=1878, message="Please enter a year in the format YYYY."),
         ],
     )
-
+    # Define submit button with a label "Add Movie"
     submit = SubmitField("Add Movie")
 
 
+# Class definition for `StringListField` which is a subclass of `TextAreaField`.
 class StringListField(TextAreaField):
+    # Method to convert the data stored in `self.data` into a string representation.
+    # Returns a string that is a concatenation of the elements in `self.data` separated by line breaks.
     def _value(self):
+        # If there is data stored in `self.data`.
         if self.data:
+            # Return a string that is a concatenation of the elements in `self.data` separated by line breaks.
             return "\n".join(self.data)
+        # If there is no data stored in `self.data`.
         else:
+            # Return an empty string.
             return ""
 
+    # Method to process the data passed in `valuelist` and store it in `self.data`.
     def process_formdata(self, valuelist):
-        # checks valuelist contains at least 1 element, and the first element isn't falsy (i.e. empty string)
+        # If `valuelist` is a non-empty list and the first element of `valuelist`
+        # is not falsy (i.e. not an empty string).
         if valuelist and valuelist[0]:
+            # Split the first element of `valuelist` by line breaks and strip whitespace from each line.
+            # Store the resulting list of strings in `self.data`.
             self.data = [line.strip() for line in valuelist[0].split("\n")]
+        # If `valuelist` is an empty list or the first element of `valuelist` is falsy.
         else:
+            # Store an empty list in `self.data`.
             self.data = []
 
 
+# Class definition for `ExtendedMovieForm` which is a subclass of `MovieForm`.
 class ExtendedMovieForm(MovieForm):
+    # Class-level attribute representing a `StringListField` for the "Cast" data.
     cast = StringListField("Cast")
+    # Class-level attribute representing a `StringListField` for the "Series" data.
     series = StringListField("Series")
+    # Class-level attribute representing a `StringListField` for the "Tags" data.
     tags = StringListField("Tags")
+    # Class-level attribute representing a `TextAreaField` for the "Description" data.
     description = TextAreaField("Description")
+    # Class-level attribute representing a `URLField` for the "Video link" data.
     video_link = URLField("Video link")
 
+    # Class-level attribute representing a `SubmitField` for the form submission button.
     submit = SubmitField("Submit")
